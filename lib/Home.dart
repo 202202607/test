93653c152977f81535bcdadf45ff75905759ec88
202202607 <ygh0795@gmail.com>
2023-05-20 13:42:38 +0900

    url

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Home extends StatelessWidget{
   const Home({Key? key}) : super(key: key);
 
+  launchTest() async{
+    launchUrl(
+      Uri.parse("https://www.foodsafetykorea.go.kr/portal/board/board.do?menu_grp=MENU_NEW05&menu_no=3852"),
+    );
+  }
+
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-            "Home",
-            style: TextStyle(
-              fontSize: 20,
-            ),
+        child:
+          ElevatedButton(
+            onPressed: () => {
+              launchTest()
+            },
+            child: const Text("url test"),
         ),
       ),
     );
