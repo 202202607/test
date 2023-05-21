1063add8e11724463cb3609d16ba596aa06a6b74
202202607 <ygh0795@gmail.com>
2023-05-22 03:29:32 +0900

    webview?

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,27 +1,79 @@
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 
-class Home extends StatelessWidget{
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+class Home extends StatefulWidget{
   const Home({Key? key}) : super(key: key);
 
-  launchTest() async{
-    launchUrl(
-      Uri.parse("https://www.foodsafetykorea.go.kr/portal/board/board.do?menu_grp=MENU_NEW05&menu_no=3852"),
-    );
+  @override
+  State<Home> createState()=>_HomeState();
+}
+
+class _HomeState extends State<Home>{
+  late final WebViewController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller=
+    WebViewController.fromPlatformCreationParams(params);
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..addJavaScriptChannel(
+        'Toaster',
+        onMessageReceived: (JavaScriptMessage message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        },
+      )
+      ..loadRequest(Uri.parse('https://www.google.com/'));
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+    _controller = controller;
   }
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(
-        child:
-          ElevatedButton(
-            onPressed: () => {
-              launchTest()
-            },
-            child: const Text("url test"),
-        ),
-      ),
+      body: WebViewWidget(controller: _controller),
+      floatingActionButton: favoriteButton(),
+    );
+  }
+
+  Widget favoriteButton() {
+    return FloatingActionButton(
+      onPressed: () async {
+        final String? url = await _controller.currentUrl();
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Favorite $url')),
+          );
+        }
+      },
+      child: const Icon(Icons.favorite),
     );
   }
 }
